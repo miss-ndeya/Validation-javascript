93a1fd29979d710f0246eb64681db2eaece4517f
unknown <miss-ndeya>
2023-08-03 00:58:30 +0200

    ajout

--- quiz.js
+++ quiz.js
@@ -1,23 +1,30 @@
+// selectionner mes elements html
 const sauter = document.getElementById("sauter");
 const score = document.getElementById("score");
 const totalScore = document.getElementById("totalscore");
 const countdown = document.getElementById("countdown");
 const set = document.querySelectorAll(".set");
 const row = document.querySelectorAll(".row input");
+
+// declaration variable
 let count = 0;
 let scoreCount = 0;
-let duration = 0;
 
+// fonction pour ajouter un ecouteur d'evenement 
 sauter.addEventListener("click", function () {
   quizz();
 });
 
+// boucle pour parcourir la liste 
 row.forEach(function (rowSingle) {
+  // evenement click sur l'input 
   rowSingle.addEventListener("click", function () {
     setTimeout(function () {
       quizz();
     }, 500);
+    // recuperer l'attribut valid
     let valid = this.getAttribute("valid");
+    // condition pour l'score
     if (valid == "valid") {
       scoreCount += 1;
       score.innerHTML = scoreCount;
@@ -30,6 +37,7 @@ row.forEach(function (rowSingle) {
   });
 });
 
+// fonction pour valider la reponse
 function quizz() {
   count += 1;
   for (let i = 0; i < set.length; i++) {
